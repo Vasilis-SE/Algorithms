2632f957981c67afe414e48dd0ccd6e49a532c64
Vasilis CodeX <vasilis.tris@gmail.com>
2020-08-12 18:18:02 +0300

    Remove & Reverse functionalities for singly list lists

--- Data Structures/Singly Linked List/singlyLinkedList.Class.js
+++ Data Structures/Singly Linked List/singlyLinkedList.Class.js
@@ -7,6 +7,19 @@ class SinglyLinkedList {
         this.tail = null;
     }
 
+    display() {
+        if(!this.head) { return console.log("Empty"); }
+
+        let displayString = "";
+        let current = this.head;
+        for(let i=0; i<this.length; i++) {
+            displayString += ` | ${i} > ${current.val} | `;
+            current = current.next;
+            if(current) displayString += ` => `;
+        }
+        console.log(displayString);
+    }
+
     push(val) {
         let node = new Node(val);
 
@@ -63,6 +76,63 @@ class SinglyLinkedList {
 
         this.length++;
         return this;
+    }
+
+    get(index) {
+        if(index >= this.length || index < 0) return null;
+
+        let current = this.head;
+        for(let i=0; i<index; i++) {
+            current = current.next;
+        }
+
+        return current;
+    }
+
+    set(index, val) {
+        let foundNode = this.get(index);
+        if(!foundNode) return false;
+        foundNode.val = val;
+        return true;
+    }
+
+    insert(index, val) {
+        if(index < 0 || index > this.length) return false;
+        if(index === 0) return this.unshift(val); // This is the same as unshift
+        if(index === this.length) return this.push(val); // This is the same as push
+            
+        let newNode = new Node(val);
+        let oldNode = this.get(index);
+        let previousNode = this.get(index - 1);
+
+        previousNode.next = newNode;
+        newNode.next = oldNode;
+        this.length++;
+        
+        return true;
+    }
+
+    remove(index) {
+        if(index < 0 || index >= this.length) return false;
+
+        if(index === 0) return this.shift(); // If the index is 0 is the same with shift.
+        if(index === (this.length - 1)) return this.pop();
+
+        let nodeToRemove = this.get(index);
+        let previousNode = this.get(index - 1);
+
+        previousNode.next = nodeToRemove.next;
+        this.length--;
+
+        return nodeToRemove;
+    }
+
+    reverse(current = this.head) {
+        if(!current) return true;
+
+        this.reverse(current.next);
+        this.push(current.val);
+        this.shift();
     }
 
 }
--- Data Structures/Singly Linked List/singlyLinkedList.js
+++ Data Structures/Singly Linked List/singlyLinkedList.js
@@ -3,8 +3,11 @@ let SinglyLinkedList = require('./singlyLinkedList.Class');
 
 let singlyLinkedList = new SinglyLinkedList();
 
-// singlyLinkedList.push(80);
-// singlyLinkedList.push(81);
+singlyLinkedList.push(80);
+singlyLinkedList.push(81);
+singlyLinkedList.push(82);
+singlyLinkedList.push(83);
+singlyLinkedList.push(84);
 // console.log(singlyLinkedList);
 
 // singlyLinkedList.pop();
@@ -13,7 +16,26 @@ let singlyLinkedList = new SinglyLinkedList();
 // singlyLinkedList.shift();
 // console.log(singlyLinkedList);
 
-singlyLinkedList.unshift(80);
-console.log(singlyLinkedList);
-singlyLinkedList.unshift(81);
-console.log(singlyLinkedList);
+// singlyLinkedList.unshift(80);
+// console.log(singlyLinkedList);
+// singlyLinkedList.unshift(81);
+// console.log(singlyLinkedList);
+
+// console.log(singlyLinkedList.get(0));
+// console.log(singlyLinkedList.get(1));
+// console.log(singlyLinkedList.get(16));
+// console.log(singlyLinkedList.get(-2));
+
+// console.log(singlyLinkedList.set(-2, 90));
+// console.log(singlyLinkedList.set(1, 90));
+// console.log(singlyLinkedList);
+
+// console.log(singlyLinkedList.display());
+// singlyLinkedList.insert(1, 90);
+// console.log(singlyLinkedList.display());
+// singlyLinkedList.insert(1, 89);
+// console.log(singlyLinkedList.display());
+
+console.log( JSON.stringify(singlyLinkedList)  );
+singlyLinkedList.reverse();
+console.log( JSON.stringify(singlyLinkedList) );
