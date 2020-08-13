5ab1be93c20854a7df45546c8ddd72cd8387a7b9
Vasilis CodeX <vasilis.tris@gmail.com>
2020-08-13 12:18:44 +0300

    Push & Pop & Shift of Doubly Linked Lists

--- Data Structures/Doubly Linked List/doublyLinkedList.Class.js
+++ Data Structures/Doubly Linked List/doublyLinkedList.Class.js
@@ -7,6 +7,18 @@ class DoublyLinkedList {
         this.length = 0;
     }
 
+    display() {
+        let displayString = "";
+        let current = this.head;
+        for(let i=0; i<this.length; i++) {
+            displayString += ` | ${i} > ${current.val} | `;
+            if(i !== this.length - 1) displayString += ` <=> `;
+            current = current.next;
+        }
+
+        console.log(displayString);
+    }
+
     push(val) {
         let newNode = new Node(val);
 
@@ -23,4 +35,43 @@ class DoublyLinkedList {
         return this;
     }
 
+    pop() {
+        if(!this.head) return false; 
+
+        let oldTail = this.tail;
+        if(this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = oldTail.previous;
+            this.tail.next = null;
+
+            // Sever the connection of the poped node to not point back to the list
+            oldTail.previous = null; 
+        }
+        
+        this.length--;
+        return oldTail;
+    }
+
+    shift() {
+        if(!this.head) return false; 
+        let oldHead = this.head;
+
+        if(this.length === 1) {
+            this.tail = null;
+            this.head = null;
+        } else {
+            // Sever the connection of the poped node to not point back to the list
+            oldHead.next = null;
+            this.head = this.head.next;
+            this.head.previous = null;
+        }
+
+        this.length--;
+        return oldHead;
+    }
+
 }
+
+module.exports = DoublyLinkedList;
--- Data Structures/Doubly Linked List/doublyLinkedList.js
+++ Data Structures/Doubly Linked List/doublyLinkedList.js
@@ -1,2 +1,14 @@
-let doublyLinkedList = require('./doublyLinkedList.js');
+let DoublyLinkedList = require('./doublyLinkedList.Class.js');
 
+let doublyLinkedList = new DoublyLinkedList();
+
+doublyLinkedList.push(80);
+doublyLinkedList.push(81);
+doublyLinkedList.push(82);
+doublyLinkedList.display();
+
+doublyLinkedList.pop();
+doublyLinkedList.display();
+
+doublyLinkedList.shift();
+doublyLinkedList.display();
