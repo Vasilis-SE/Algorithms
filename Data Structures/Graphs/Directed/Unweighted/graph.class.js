1ed1383fbf70500d432129cd0827bba952902f38
Vasilis CodeX <vasilis.tris@gmail.com>
2020-11-12 21:34:40 +0200

    vertex, getvertex, addedge functions on directed unweighted graph

--- Data Structures/Graphs/Directed/Unweighted/graph.class.js
+++ Data Structures/Graphs/Directed/Unweighted/graph.class.js
@@ -1,6 +1,42 @@
+class Vertex {
+    constructor(id) {
+        this.id = id;
+        this.adjacent = {}
+    }
+
+    getConnections() {
+        return Object.keys(this.adjacent);
+    }
+
+    addAdjecentVertex(id, vrtx) {
+        this.adjacent[id] = vrtx;
+        return true;
+    }
+
+}
+
 class Graph {
     constructor() {
-        this.adjacencyList = {};
+        this.vrtxList = {};
+    }
+
+    addVertex(id) {
+        if(typeof this.vrtxList[id] != undefined) return false;
+        let newVrtx = new Vertex(id);
+        this.vrtxList[id] = newVrtx;
+        return true;
+    }
+
+    getVertex(id) {
+        if(typeof this.vrtxList[id] == undefined) return false;
+        return this.vrtxList[id];
+    }
+
+    addEdge(fromVrtxID, toVrtxID) {
+        if(typeof this.vrtxList[fromVrtxID].getConnections().contains(toVrtxID) != undefined) return false;
+        let toVrtx = this.getVertex(toVrtxID);
+        this.vrtxList[fromVrtxID].addAdjecentVertex(toVrtxID, toVrtx);
+        return true;
     }
 
 } // End of class
