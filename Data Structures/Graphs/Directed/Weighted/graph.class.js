66b5f26b95d0ad11d5abee185b0c0569fef71507
Vasilis CodeX <vasilis.tris@gmail.com>
2020-11-21 09:53:52 +0200

    D.W graph get connections & vertexes

--- Data Structures/Graphs/Directed/Weighted/graph.class.js
+++ Data Structures/Graphs/Directed/Weighted/graph.class.js
@@ -1,12 +1,32 @@
 class Vertex {
     constructor(id) {
         this.id = id;
+        this.weight = 0;
         this.adjacent = {}
     }
+
+    getConnections() {
+        return Object.keys(this.adjacent);
+    }
+
+
+
 }
 
 class Graph {
     constructor() {
         this.vertexList = {};
     }
+
+    addVertex(id) {
+        if(typeof this.vrtxList[id] != 'undefined') return false;
+        let newVrtx = new Vertex(id);
+        this.vrtxList[id] = newVrtx;
+        return true;
+    }
+
+
 } // End of class
+
+
+module.exports = new Graph();
---
+++ Data Structures/Graphs/Directed/Weighted/graph.example.js
@@ -0,0 +1,10 @@
+const graph = require('./graph.class');
+
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D'); 
+graph.addVertex('E');
+graph.addVertex('F');
+graph.addVertex('G');
+
