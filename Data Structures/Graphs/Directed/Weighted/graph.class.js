7467e8b1f97bd96947d086a31d6af689943fb28a
Vasilis CodeX <vasilis.tris@gmail.com>
2020-11-21 10:07:58 +0200

    D.W graph adding edges with weight

--- Data Structures/Graphs/Directed/Weighted/graph.class.js
+++ Data Structures/Graphs/Directed/Weighted/graph.class.js
@@ -1,7 +1,6 @@
 class Vertex {
     constructor(id) {
         this.id = id;
-        this.weight = 0;
         this.adjacent = {}
     }
 
@@ -9,8 +8,10 @@ class Vertex {
         return Object.keys(this.adjacent);
     }
 
-
-
+    addAdjecentVertex(id, vrtx, weight) {
+        this.adjacent[id] = {'vertex': vrtx, 'weight': weight};
+        return true;
+    }
 }
 
 class Graph {
@@ -19,11 +20,26 @@ class Graph {
     }
 
     addVertex(id) {
-        if(typeof this.vrtxList[id] != 'undefined') return false;
+        if(typeof this.vertexList[id] != 'undefined') return false;
         let newVrtx = new Vertex(id);
-        this.vrtxList[id] = newVrtx;
+        this.vertexList[id] = newVrtx;
         return true;
     }
+
+    getVertex(id) {
+        if(typeof this.vertexList[id] == 'undefined') return false;
+        return this.vertexList[id];
+    }
+
+    addEdge(fromVrtxID, toVrtxID, weight) {
+        if(toVrtxID in this.vertexList[fromVrtxID].getConnections()) return false;
+        
+        let toVrtx = this.getVertex(toVrtxID);
+        this.vertexList[fromVrtxID].addAdjecentVertex(toVrtxID, toVrtx, weight);
+        return true;
+    }
+
+
 
 
 } // End of class
