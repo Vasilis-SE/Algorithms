6b4eec345bd4170e8b1e51ba732450b2011fbfa3
Vasilis CodeX <vasilis.tris@gmail.com>
2020-11-21 09:42:55 +0200

    D.W graph class constructors

---
+++ Data Structures/Graphs/Directed/Weighted/graph.class.js
@@ -0,0 +1,12 @@
+class Vertex {
+    constructor(id) {
+        this.id = id;
+        this.adjacent = {}
+    }
+}
+
+class Graph {
+    constructor() {
+        this.vertexList = {};
+    }
+} // End of class
