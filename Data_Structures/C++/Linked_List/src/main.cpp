63e8a4c11c90be18f2f9b22dff0fe1538fc7e6c7
Vasilis CodeX <vasilis.tris@gmail.com>
2022-10-28 17:56:51 +0300

    new datatypes check support

--- Data_Structures/C++/Linked_List/src/main.cpp
+++ Data_Structures/C++/Linked_List/src/main.cpp
@@ -8,42 +8,59 @@ int main() {
     lli.add(4);
     lli.display();
 
-    // Char data type
-    LinkedList<char> llc;
-    llc.add('A');
-    llc.add('B');
-    llc.display();
+    // // Char data type
+    // LinkedList<char> llc;
+    // llc.add('A');
+    // llc.add('B');
+    // llc.display();
 
-    // Double data type
-    LinkedList<double> lld;
-    lld.add(1.56);
-    lld.add(4.37);
-    lld.display();
+    // // Double data type
+    // LinkedList<double> lld;
+    // lld.add(1.56);
+    // lld.add(4.37);
+    // lld.display();
 
-    // Float data type
-    LinkedList<float> llf;
-    llf.add(1.56);
-    llf.add(4.37);
-    llf.display();
+    // // Float data type
+    // LinkedList<float> llf;
+    // llf.add(1.56);
+    // llf.add(4.37);
+    // llf.display();
 
-    // C++ string std data type
-    LinkedList<std::string> lls1;
-    lls1.add("Hello");
-    lls1.add("World");
-    lls1.display();
+    // // C++ string std data type
+    // LinkedList<std::string> lls1;
+    // lls1.add("Hello");
+    // lls1.add("World");
+    // lls1.display();
 
-    // C/C++ string literal (deprecated)
-    char *pc;
-    pc = "Hello C/C++ World !!";
-    LinkedList<char*> llcps;
-    llcps.add(pc);
-    llcps.display();
+    // // C/C++ string literal to pointer (deprecated)
+    // char *p_sl;
+    // p_sl = "Hello C/C++ world !!";
+    // LinkedList<char*> llpsl;
+    // llpsl.add(p_sl);
+    // llpsl.display();
 
-    // C styled array of chars as string data type with pre-defined size
+    // // C/C++ string literal to array (deprecated)
+    // char sla[26] = "String literal to array!!";
+    // LinkedList<char*> llsla;
+    // llsla.add(sla);
+    // llsla.display();
+
+    // // C/C++ constant string literal to pointer
+    // char const *p_csl = "Constant string literal to array!!";
+    // LinkedList<const char*> llcpsl;
+    // llcpsl.add(p_csl);
+    // llcpsl.display();
+
+    // // C styled array of chars as string data type with pre-defined size
     // char hello[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
-    // LinkedList<char[6]> llcs;
+    // LinkedList<char*> llcs;
     // llcs.add(hello);
     // llcs.display();
+
+    // int ia[6] = {1,2,3,4,5};
+    // LinkedList<int*> llia;
+    // llia.add(ia);
+    // llia.display();
 
     return 0;
 }
